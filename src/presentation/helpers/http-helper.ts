d3880a1ab2f08d951fec4ab8330664edb1de1f2e
Rodrigo Souza <rodrigo-w-s@hotmail.com>
2022-03-15 11:17:40 -0300

    refactor: move all signup protocols to a single file

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,4 +1,4 @@
-import { HttpResponse } from '@/protocols/http'
+import { HttpResponse } from '@/presentation/protocols/http'
 import { ServerError } from '../errors/server-error'
 
 export const badRequest = (error: Error): HttpResponse => ({
