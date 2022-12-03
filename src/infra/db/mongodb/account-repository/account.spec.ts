22de3e1136f887d2ded8760e0b275ce2ed383300
Rodrigo Souza <rodrigo-w-s@hotmail.com>
2022-12-03 15:41:24 -0300

    refactor: adjust grammar

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -9,12 +9,12 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
-  const mekeSut = (): AccountMongoRepository => {
+  const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository()
   }
 
   test('Should return an account on success', async () => {
-    const sut = mekeSut()
+    const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
