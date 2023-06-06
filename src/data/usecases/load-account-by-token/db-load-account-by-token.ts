fa8abbcab19fffb9f47454ad077340e54017c22f
RodrigoWSouza <rodrigo-w-s@hotmail.com>
2023-06-06 16:23:42 -0300

    refactor: apply return early pattern

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -14,12 +14,15 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
 
   async load (accessToken: string, role?: string): Promise<AccountModel> {
     const token = await this.decrypter.decrypt(accessToken)
-    if (token) {
-      const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
-      if (account) {
-        return account
-      }
+    if (!token) {
+      return null
     }
-    return null
+
+    const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
+    if (!account) {
+      return null
+    }
+
+    return account
   }
 }
