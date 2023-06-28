a81ebe14b14a8612721cd4f3e0a48ad33dc0a614
RodrigoWSouza <rodrigo-w-s@hotmail.com>
2023-06-28 15:00:32 -0300

    feat: ensure save result route returns 200 on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -6,22 +6,23 @@ import {
 import { SurveyModel } from '@/domain/models'
 import { AddSurveyModel } from '@/domain/usecases'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { ObjectId } from 'mongodb'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
-    const accountCollection = await MongoHelper.getCollection('surveys')
-    await accountCollection.insertOne(surveyData)
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.insertOne(surveyData)
   }
 
   async loadAll (): Promise<SurveyModel[]> {
-    const accountCollection = await MongoHelper.getCollection('surveys')
-    const surveys = await accountCollection.find().toArray()
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
     return MongoHelper.mapCollection(surveys)
   }
 
   async loadById (id: string): Promise<SurveyModel> {
-    const accountCollection = await MongoHelper.getCollection('surveys')
-    const survey = await accountCollection.findOne({ _id: id })
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const survey = await surveyCollection.findOne({ _id: new ObjectId(id) })
     return survey && MongoHelper.map(survey)
   }
 }
