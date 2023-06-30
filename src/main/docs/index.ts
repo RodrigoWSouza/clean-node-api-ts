9d8be2c5697161b24345861e2a03ec17b462fee7
RodrigoWSouza <rodrigo-w-s@hotmail.com>
2023-06-30 15:41:53 -0300

    docs: add docs to login route

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,3 +1,7 @@
+import components from './components'
+import paths from './paths'
+import schemas from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
@@ -24,5 +28,8 @@ export default {
   }, {
     name: 'Enquete',
     description: 'APIs relacionadas a Enquete'
-  }]
+  }],
+  paths,
+  schemas,
+  components
 }
