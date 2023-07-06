fac2bdd4206faacdd1e53eddb987f034a8993b35
RodrigoWSouza <rodrigo-w-s@hotmail.com>
2023-07-06 14:49:30 -0300

    feat: ensure LoadSurveyResultController returns 500 if LoadSurveyResult throws

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -66,4 +66,13 @@ describe('LoadSurveyResult Controller', () => {
 
     expect(loadSurveyByIdSpy).toHaveBeenCalledWith(httpRequest.params.surveyId)
   })
+
+  test('Should return 500 if LoadSurveyResult throws', async () => {
+    const { sut, loadSurveyResultStub } = makeSut()
+    jest.spyOn(loadSurveyResultStub, 'load').mockImplementationOnce(throwError)
+
+    const httpResponse = await sut.handle(mockRequest())
+
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
